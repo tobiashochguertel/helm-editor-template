84eb6a7025fc48eff3b434268ae1a6ebad5491ed
Tobias Hochgürtel <tobias.hochguertel@googlemail.com>
2023-06-24 19:21:00 +0200

    Add new build steps

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # podman build --format docker --tag th-helm-playground-template .
 
-docker build --tag th-helm-playground-template:1.0.1 .
-docker tag th-helm-playground-template:1.0.1 th-helm-playground-template:latest
+# docker build --tag docker.io/tobiashochguertel/th-helm-playground-template:1.0.1 .
+# docker tag docker.io/tobiashochguertel/th-helm-playground-template:1.0.1 docker.io/tobiashochguertel/th-helm-playground-template:latest
 
+# docker push docker.io/tobiashochguertel/th-helm-playground-template:1.0.1
+# docker push docker.io/tobiashochguertel/th-helm-playground-template:latest
+
+docker buildx build --push \
+  --platform linux/arm64/v8,linux/amd64 \
+  --tag docker.io/tobiashochguertel/th-helm-playground-template:1.0.1 \
+  --tag docker.io/tobiashochguertel/th-helm-playground-template:latest \
+  .
