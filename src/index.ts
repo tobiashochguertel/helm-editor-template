ba680d6b9e207457970263845fa156db3709a69d
Tobias Hochguertel <tobias.hochguertel@googlemail.com>
2023-10-30 20:27:46 +0100

    Improve template rendering and reopen a template via url.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import express, { Express } from 'express';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
-import { exec } from "child_process";
+import { exec, execSync } from "child_process";
 // import jsYaml from 'js-yaml';
 import fs from 'fs';
 const jsYaml = require('js-yaml');
-
+import http from 'http';
 
 dotenv.config();
+const port = process.env.PORT || 5521;
+const hostname = process.env.HOSTNAME || "localhost";
 
 type ChartYaml = {
   apiVersion: string;
@@ -25,19 +27,20 @@ type Content = {
 }
 
 const app: Express = express()
-app.use(bodyParser.text({ limit: '50mb' }));
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ extended: false, limit: '50mb' }));
-app.use(express.static('public'));
+app.use(bodyParser.text({ limit: '200mb' }));
+app.use(bodyParser.json({ limit: '200mb' }));
+app.use(bodyParser.urlencoded({ extended: false, limit: '200mb' }));
 
+// POST method route: /template
 app.post('/template', async (req, res) => {
-  fs.rmSync("./public/chart/", { recursive: true, force: true });
+  // fs.rmSync("./public/chart/", { recursive: true, force: true });
   const chart = new Map<string, Content>();
   for (const value in req.body.chart) {
     chart.set(value, req.body.chart[value]);
   }
   const directories = Array.from(chart.values()).filter((item: Content) => item.type === "directory");
   const files = Array.from(chart.values()).filter((item: Content) => item.type === "file");
+  const charts = Array.from(chart.values()).filter((item: Content) => item.type === "charts");
   directories.forEach((directory: Content) => {
     try {
       fs.mkdirSync(`./public/chart/${directory.filename}`, { recursive: true });
@@ -51,7 +54,6 @@ app.post('/template', async (req, res) => {
     }
   });
   files.forEach((file: Content) => {
-
     try {
       fs.writeFileSync(`./public/chart/${file.filename}`, file.content ?? "");
     } catch (err: unknown) {
@@ -63,14 +65,27 @@ app.post('/template', async (req, res) => {
       }
     }
   });
+  charts.forEach((chart) => {
+    const file = fs.createWriteStream(`./public/chart/${chart.filename}`);
+    http.get(`${chart.content}`, function (response) {
+      response.pipe(file);
+
+      // after download completed close filestream
+      file.on("finish", () => {
+        file.close();
+        console.log("chart.content", chart.content);
+        console.log("chart.filename", chart.filename);
+        console.log("Download Completed");
+      });
+    });
+  })
 
   const selection: Content = req.body.selection as Content;
-  fs.writeFileSync("./public/chart/" + selection.filename, selection.content || "");
+  fs.writeFileSync("./public/chart/" + selection.filename, selection.content ?? "");
 
   const myValues: string = req.body.myValues;
   fs.writeFileSync("./public/myValues.yaml", myValues || "");
 
-  // helm template . -f ../myValues.yaml
   exec("helm template ./public/chart -f ./public/myValues.yaml", (error, stdout, stderr) => {
     if (error || stderr) {
       if (error !== null)
@@ -94,7 +109,6 @@ app.post('/template', async (req, res) => {
     let result = "";
     const parsed = jsYaml.loadAll(withSourceOutput);
     parsed.forEach((item: any) => {
-      console.log(item.Source);
       if (item['Source'] === selection.filename) {
         // Remove Source field, which we added to know which content belongs to which file.
         delete item.Source;
@@ -103,11 +117,11 @@ app.post('/template', async (req, res) => {
       }
     });
     res.send(result);
-    // fs.rmSync("./public/chart/", { recursive: true, force: true });
   });
   // res.json({ "result": "ok" });
 });
 
+// GET method route: /template
 app.get('/template', async (req, res) => {
   // helm template . -f ../myValues.yaml
   exec("helm template ./public/chart -f ./public/myValues.yaml", (error, stdout, stderr) => {
@@ -129,15 +143,15 @@ app.get('/template', async (req, res) => {
     const sourceRegexp = new RegExp(`# Source: ${chart.name}\/`, "g");
     let withSourceOutput = rawYamlOutput.replaceAll(sourceRegexp, "# Source: ");
     withSourceOutput = withSourceOutput.replaceAll("# Source:", "Source:");
-    // console.log(`withSourceOutput: ${withSourceOutput}`);
 
     const parsed = jsYaml.loadAll(withSourceOutput);
-    // console.log(parsed);
     res.json(parsed);
   });
 });
 
-const port = process.env.PORT || 5521;
 app.listen(port, () => {
+  console.log("Cleaning up...")
+  fs.rmSync("./public/chart/", { recursive: true, force: true });
+  console.log("Cleaning up... DONE")
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
